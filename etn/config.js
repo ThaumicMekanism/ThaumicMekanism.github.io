c856a32edd48e6a50b88e940e8741abf5cedcb61
Stephan K <thaumicmekanism@gmail.com>
2018-01-21 09:39:30 -0800

    Update config.js

--- etn/config.js
+++ etn/config.js
@@ -1,9 +1,9 @@
-var api = "http://18.221.92.44:8117";
+var api = "http://18.221.149.215:8117";
 
 var coinUnits = 100;
 var coinDecimalPlaces = 2;
 
-var poolHost = "18.221.92.44";
+var poolHost = "18.221.149.215";
 
 var irc = "irc.freenode.net/#monero-pools";
 
