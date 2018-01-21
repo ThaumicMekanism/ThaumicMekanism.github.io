06990b39930eb69f1adac5d472ead78b08e3ec31
Stephan K <thaumicmekanism@gmail.com>
2018-01-20 22:46:12 -0800

    ETN Test stuff

---
+++ etn/config.js
@@ -0,0 +1,16 @@
+var api = "http://18.221.92.44:8117";
+
+var coinUnits = 100;
+var coinDecimalPlaces = 2;
+
+var poolHost = "18.221.92.44";
+
+var irc = "irc.freenode.net/#monero-pools";
+
+var email = "support@electroneum.com";
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "https://blockexplorer.electroneum.com/block/";
+
+var transactionExplorer = "https://blockexplorer.electroneum.com/tx/";
